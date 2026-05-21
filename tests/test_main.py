7fae4f50e27cdb2387712eb10974d7368438e161
Pooja <poojaambekar99@gmail.com>
2026-05-21 15:35:39 +0400

    Add API tests using pytest

---
+++ tests/test_main.py
@@ -0,0 +1,21 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_home():
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Career Compass AI running"
+    }
+def test_invalid_file_type():
+
+    response = client.post(
+        "/upload-resume",
+        files={"file": ("image.jpg", b"fake", "image/jpeg")}
+    )
+
+    assert response.status_code == 400
